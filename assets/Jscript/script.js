d668992738fd3179f28b8575bcb84f8856f267fa
MUGESH MANOHARAN <samcharlesmugesh@gmail.com>
2024-06-15 09:53:26 +0530

    updated css

--- assets/Jscript/script.js
+++ assets/Jscript/script.js
@@ -3,12 +3,16 @@
  function DownloadBtn() {
 
     //  Get the element with the class "Download_CV" and change its innerHTML to "Download Completed"
-    document.querySelector(".Download_CV").innerHTML="Download Completed";
+    document.querySelector(".Download_CV").innerHTML="Download Sucessfully";
 
     // Function to display an alert message
     setTimeout ( () => {
         alert("CV downloaded successfully!"); 
         },2000); 
+
+     setTimeout(() => {
+       document.querySelector(".Download_CV").innerHTML = "Download CV";
+    }, 8000); 
       
       
 }
