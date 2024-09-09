c3baf9c24c49963dd3f4220fda6879c8e79cde1c
MUGESH MANOHARAN <samcharlesmugesh@gmail.com>
2024-09-09 20:52:51 +0530

    updated

--- assets/Jscript/script.js
+++ assets/Jscript/script.js
@@ -1,18 +1,60 @@
+// NavBar 
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  const observerOptions = {
+    root: null, // relative to the viewport
+    threshold: 0.7, // 70% of the section is in view
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const sectionID = entry.target.getAttribute("id");
+      if (entry.isIntersecting) {
+        // Remove active class from all links
+        navLinks.forEach((link) => link.classList.remove("active"));
+
+        // Add active class to the current link
+        document
+          .querySelector(`a[href="#${sectionID}"]`)
+          .classList.add("active");
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => observer.observe(section));
+});
+
 
 // Function to handle the download button click event        
  function DownloadBtn() {
-
-    //  Get the element with the class "Download_CV" and change its innerHTML to "Download Completed"
     document.querySelector(".Download_CV").innerHTML="Download Sucessfully";
-
-    // Function to display an alert message
     setTimeout ( () => {
         alert("CV downloaded successfully!"); 
         },2000); 
-
      setTimeout(() => {
        document.querySelector(".Download_CV").innerHTML = "Download CV";
-    }, 8000); 
-      
-      
+    }, 8000);  
 }
+
+// ShowCase
+function toggleClassBasedOnWidth() {
+  const element = document.getElementById("ShowCase"); // Replace with the selector of your element
+
+  if (window.innerWidth < 768) {
+     element.classList.add("row");
+     element.classList.remove("d-flex ", "flex-row", "overflow-x-scroll");
+  } else if (window.innerWidth >= 768) {
+   element.classList.add("d-flex", "flex-row", "overflow-x-scroll");
+   element.classList.remove("row");
+  }
+}
+toggleClassBasedOnWidth();
+window.addEventListener("resize", toggleClassBasedOnWidth);
+
+// AOS
+  AOS.init({
+   once:true,
+   mirror:false,
+  });
---
+++ assets/Jscript/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".card-header").slick({
+    slidesToShow: 1,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    draggable: true,
+  });
+});
